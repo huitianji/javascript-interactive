6201ca2ae65c1c42812e3006745f53f41bd54f3c
吉会田 <jihuitian@souyidai.com>
2016-03-29 14:57:49 +0800

    ajax基础学习

---
+++ js-case/js/new-ajax.js
@@ -0,0 +1,25 @@
+function ajax(url,oRequest,fnSucc,fnFail){
+    //1.创建ajax对象
+    var oAjax = null;
+    if(window.XMLHttpReques){
+        oAjax = new XMLHttpReques();
+    }else{
+        oAjax = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    //2.链接服务器
+    oAjax.open(oRequest,url,true);
+    //3.发送请求
+    oAjax.send();
+    //4.接收请求
+    oAjax.onreadystatechange = function(){
+        if(oAjax.readyState == 4){
+
+            if(oAjax.status == 200){
+                fnSucc(oAjax.responseText);
+            }else{
+                fnFail(oAjax.status);
+            }
+
+        }
+    }
+}
